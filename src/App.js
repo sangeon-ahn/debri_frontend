3ffe3d2bf0445e11d15d4f32eedc66df54ca1ec9
chljinsil <jinsil378900@gmail.com>
2022-07-01 20:17:23 +0900

    Test : login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <p>sumin</p>
+      <Login></Login>
     </div>
   );
 }
