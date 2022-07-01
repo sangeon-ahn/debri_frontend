30b78badcaa56c2d1a994e7963913c93492e7243
chljinsil <jinsil378900@gmail.com>
2022-07-02 05:38:36 +0900

    Feat : account

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
-
-
-
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Account from "./pages/Account/Account";
+import Timetable from "./pages/Timetable/Timetable";
 
 function App() {
   return (
@@ -11,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="home" element={<Home />} />
+        <Route path="account" element={<Account />} />
+        <Route path="timetable" element={<Timetable />} />
       </Routes>
     </BrowserRouter>
   );
