b0c8eea373613f6b5d7a6d671796d39bb6500060
AhnSangEon <stdleaf@gmail.com>
2022-07-01 17:17:16 +0900

    Feat : 로그인 시 닉네임 표시

--- js/login.js
+++ js/login.js
@@ -14,41 +14,59 @@ const dataSet = [
 ];
 
 const outA = document.querySelector("#A13_out");
-	const loginForm = document.querySelector("#A13_form")
-	const loginInput = document.querySelector(".A13_block input");
-	const logOut = document.querySelector("#A13_logout");
-	const usrName = localStorage.getItem("username");
+const loginForm = document.querySelector("#A13_form");
+const loginInput = document.querySelector(".A13_block input");
+const logOut = document.querySelector("#A13_logout");
+const usrName = localStorage.getItem("username");
+const passwordInput = document.querySelector("#input-password");
 
-	function onLoginSubmit(event) {
-		loginForm.classList.add("hidden");
-		loginForm.classList.remove("aaa");
-		const username = loginInput.value;
-		localStorage.setItem("username", username);
-		paintHello(username);
+function onLoginSubmit(event) {
+	event.preventDefault();
+	
+	const username = loginInput.value;
+	const password = passwordInput.value;
+	
+	if (!username) {
+		alert('input username');
+		return;
 	}
 
-	function paintHello(username) {
-		outA.innerText = `Hello ${username}!`;
-		outA.classList.remove("hidden");
-		logOut.classList.remove("hidden");
+	const userData = dataSet.filter(data => data.id === username)[0];
+	
+	if (!userData || userData.pw !== password) {
+		alert('input correct account info');
+		return;
 	}
+	
+	loginForm.classList.add("hidden");
+	loginForm.classList.remove("aaa");
+	paintHello(userData.nickname);
+}
 
-	function logOutFunc() {
-		if  (usrName !== null) {
-			localStorage.removeItem("username");
-			logOut.classList.add("hidden");
-			outA.classList.add("hidden");
-			loginForm.classList.remove("hidden");
-			loginForm.classList.add("aaa");
-		}
-	}
+function paintHello(username) {
+	outA.innerText = `Hello ${username}!`;
+	outA.classList.remove("hidden");
+	logOut.classList.remove("hidden");
+}
 
-	if  (usrName === null) {
-		loginForm.classList.remove("hidden");
-		loginForm.classList.add("aaa");
-		logOut.classList.add("hidden");
-		loginForm.addEventListener("submit", onLoginSubmit);
-	} else {
-		paintHello(usrName);
-		logOut.addEventListener("click", logOutFunc);
-	}
+function logOutFunc() {
+	console.log('logout');
+	loginInput.value = '';
+	passwordInput.value = '';
+	// localStorage.removeItem("username");
+	logOut.classList.add("hidden");
+	outA.classList.add("hidden");
+	loginForm.classList.remove("hidden");
+	loginForm.classList.add("aaa");
+}
+
+if  (usrName === null) {
+	// console.log('hi');
+	loginForm.classList.remove("hidden");
+	loginForm.classList.add("aaa");
+	logOut.classList.add("hidden");
+	loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+	console.log('bye');
+}
+logOut.addEventListener("click", logOutFunc);
