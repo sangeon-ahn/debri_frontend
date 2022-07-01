6541044b21935676b8e7769f1283f0e883e9a32d
chljinsil <jinsil378900@gmail.com>
2022-07-01 22:33:50 +0900

    Feat : account

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Login from "./pages/Login/Login";
+import Account from "./pages/Account/Account";
 
 function App() {
   return (
     <div className="App">
       <Login></Login>
+      <Account></Account>
     </div>
   );
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Login = ()=>{
 
-    return(
-      <div>
-        난 로그인
-      </div>
-    )
+  return(
+    <div>
+      난 로그인
+    </div>
+  )
 }
 export default Login;
