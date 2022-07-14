559298f2c6d03271798c9ba27734f3286cdb86cd
stdleaf <stdleaf@gmail.com>
2022-07-14 23:42:14 +0900

    포스팅 취소 모달 트랜지션 구현

--- src/pages/PostWritePage/PostWriteCancelModal/PostWriteCancelModal.js
+++ src/pages/PostWritePage/PostWriteCancelModal/PostWriteCancelModal.js
@@ -1,30 +1,32 @@
 import './PostWriteCancelModal.css';
 import alertIcon from '../../../assets/alertIcon.png';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
 
 export default function PostWriteCancelModal(props) {
   console.log(props);
   const { isCancelModalOpen, closeCancelModal } = props;
-  
+  const navigate = useNavigate();
+
+  // 닫힐 때 css 반영이 끝난 이후에 return되어야 함
+  // --> setTimeout 사용
+  // if (!isCancelModalOpen) {
+  //   let timerId = setTimeout(() => {
+  //     console.log('hi');
+  //   }, 500);
+  // };
+
+  const isFirst = useRef(true);
+
+  useEffect(() => {
+    if (isFirst.current) {
+      isFirst.current = false;
+    }
+  }, []);
+
   return (
-    // <div className={isCancelModalOpen ? 'open-cancel-modal modal' : "modal" }>
-    //   { isCancelModalOpen ? (
-    //   <div className='post-write-cancel-modal'>
-        // <div className='cancel-title'>
-        //   <div className='alert-icon-box'>
-        //     <img src={alertIcon} alt='' className='alert-icon' />
-        //   </div>
-        //   <span>정말 작성을 취소하시겠어요?</span>
-        // </div>
-        // <div className='yesno-box'>
-        //   <button className='cancel-yes-button'>네</button>
-        //   <button className='cancel-no-button' onClick={() => setIsCancelModalOn(false)}>아니오</button>
-    //     </div>
-    //   </div>) : null
-    //   }
-    // </div>
-    <div className={isCancelModalOpen ? 'modal openModal' : 'modal'}>
-      {isCancelModalOpen ? (
-        <div className='post-write-cancel-modal'>
+    <div className={isCancelModalOpen ? 'modal openModal' : isFirst.current ? 'modal display-none' : 'modal closeModal'}>
+        <div className={isCancelModalOpen ? 'post-write-cancel-modal modal-show': 'post-write-cancel-modal modal-close'}>
           <div className='cancel-title'>
             <div className='alert-icon-box'>
               <img src={alertIcon} alt='' className='alert-icon' />
@@ -32,11 +34,10 @@ export default function PostWriteCancelModal(props) {
           <span>정말 작성을 취소하시겠어요?</span>
           </div>
           <div className='yesno-box'>
-            <button className='cancel-yes-button'>네</button>
+            <button className='cancel-yes-button' onClick={() => navigate(-1)}>네</button>
             <button className='cancel-no-button' onClick={() => closeCancelModal()}>아니오</button>
           </div>
         </div>
-      ) : null}
     </div>
   );
 }
