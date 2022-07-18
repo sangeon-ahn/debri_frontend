6fbc94e0dcb12993391c0909bab1a43820fdd894
jinsil <jinsil378900@gmail.com>
2022-07-18 21:04:41 +0900

    Feat : 회원가입 완성

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -15,17 +15,18 @@ const Account =()=>{
   const [allCheck, setAllCheck] = useState(false);
   const [useCheck, setUseCheck] = useState(false);
   const [marketingCheck, setMarketingCheck] = useState(false);
+  const [login, setLogin] = useState(false);
 
   function onClickSave(event){
     event.preventDefault() 
+    setLogin(true)
     if(Password !== PasswordCheck) {
       return alert('비밀번호와 비밀번호확인은 같아야 합니다.')
     } else if (!Id || !Password || !Nickname|| !Birth || !useCheck){
-      alert("필수항목을 확인해주세요")
+      console.log('fail')
     } else {
-      // postData(Id, Password, Birth, Nickname);
+      postData(Id, Password, Birth, Nickname);
     }
-    console.log(typeof(Birth))
     console.log(Id, Password, Birth, Nickname);
   }     
 
@@ -80,16 +81,14 @@ const Account =()=>{
       setUseCheck(false);
       setMarketingCheck(false);
     } 
-  };
-  
+  };  
   const useBtnEvent =()=>{
     if(useCheck === false) {
       setUseCheck(true)
     }else {
       setUseCheck(false)
     }
-  };
-  
+  };  
   const marketingBtnEvent =()=>{
     if(marketingCheck === false) {
       setMarketingCheck(true)
@@ -109,48 +108,49 @@ const Account =()=>{
   return (
     <div className='account'>
       <button><Link to="/">돌아가기</Link></button>
-      <div className='Logo_box'>
+      <div className= 'Logo_box'>
         <img src={onlylogo} alt="데브리" className="onlylogo"></img>
         <img src={logoType} alt="데브리" className="logotype"></img>
         <p className='logotext'>“개발과 관련된 모든 것들을 연결합니다.”</p>
       </div>
 
       <div className='account_warp'>
-        <div className='account_content'>
-          ID <input type="email" placeholder="email 형식" onChange={onChangeId} value={Id}/>
+        <div className= {`account_content ${!login ? '' :( Id ? 'success' : 'fail')}`}>
+          ID <input className='textinput' type="email" placeholder="email 형식" onChange={onChangeId} value={Id}/>
         </div>
-        <div className='account_content'>
-          PW <input type="password" placeholder="비밀번호" onChange={onChangePassword} value={Password} />
+        <div className= {`account_content ${!login ? '' :( Password ? 'success' : 'fail')}`}>
+          PW <input className='textinput' type="password" placeholder="비밀번호" onChange={onChangePassword} value={Password} />
         </div>
-        <div className='account_content'>
-          비밀번호<br></br>확인 <input type="password" placeholder="비밀번호 확인" onChange={onChangePasswordCheck} value={PasswordCheck} />
+        <div className= {`account_content ${!login ? '' :( PasswordCheck ? 'success' : 'fail')}`}>
+          비밀번호 확인 <input className='textinput' type="password" placeholder="비밀번호 확인" onChange={onChangePasswordCheck} value={PasswordCheck} />
         </div>
-        <div className='account_content'>
-          생년<br></br>월일 <input type="date" onChange={onChangeBirth} value={Birth}/>
+        <div className= {`account_content ${!login ? '' :( Birth ? 'success' : 'fail')}`}>
+          생년월일<input className='textinput' type="date" onChange={onChangeBirth} value={Birth}/>
         </div>
-        <div className='account_content'>
-          닉네임 <input type="text" placeholder="닉네임" onChange={onChangeNickname} value={Nickname} />
+        <div className= {`account_content ${!login ? '' :( Nickname ? 'success' : 'fail')}`}>
+          닉네임 <input className='textinput' type="text" placeholder="닉네임" onChange={onChangeNickname} value={Nickname} />
         </div>
-        <button onClick={onClickSave}>시작하기</button>
+        <button className={`start_btn ${!login ? '' :(useCheck ? 'success' : 'fail')}`} onClick={onClickSave}>시작하기</button>
 
         <form method="post" action="">
           <div>
             <div>
-              <div>
-                <input type="checkbox" id="all-check" checked={allCheck} onChange={allBtnEvent}/>
-                <label for="all-check">전체 약관에 동의합니다.</label>
+              <div className={`agree_btn ${!login ? '' :(useCheck ? 'success' : 'fail')}`}>
+                <input className='textinput' type="checkbox" id="all-check" checked={allCheck} onChange={allBtnEvent}/>
+                <label for="all-check"><div className='agree_text'>전체 약관에 동의합니다.</div></label>
               </div>
-              <div>
-                <input type="checkbox" id="check2" checked={useCheck}  onChange={useBtnEvent}/>
-                <label for="check2">개인정보 약관에 동의합니다. (필수)</label>
+              <div className={`agree_btn ${!login ? '' :(useCheck ? 'success' : 'fail')}`}>
+                <input className='textinput' type="checkbox" id="check2" checked={useCheck}  onChange={useBtnEvent}/>
+                <label for="check2"><div className='agree_text'>개인정보 약관에 동의합니다. (필수)</div></label>
               </div>
-              <div>
-                <input type="checkbox" id="check3" checked={marketingCheck}  onChange={marketingBtnEvent}/>
-                <label for="check3">서비스 홍보 약관에 동의합니다. (선택)</label>
+              <div className='agree_btn'>
+                <input className='textinput' type="checkbox" id="check3" checked={marketingCheck}  onChange={marketingBtnEvent}/>
+                <label for="check3"><div className='agree_text'>서비스 홍보 약관에 동의합니다. (선택)</div></label>
               </div>
             </div>
           </div>
         </form>
+        
       </div>
     </div>
 
