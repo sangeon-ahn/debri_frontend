127a9c1db64c2cc7de12caa707e5a4945b0da817
jinsil <jinsil378900@gmail.com>
2022-07-23 13:09:42 +0900

    Feat : postwrite api 연결

--- src/pages/PostWritePage/PostWritePage.js
+++ src/pages/PostWritePage/PostWritePage.js
@@ -5,24 +5,52 @@ import pencil from '../../assets/pencil.png';
 import writePost from '../../assets/writePost.png';
 import selectButton from '../../assets/selectButton.png';
 import leftArrow from '../../assets/leftArrow.png';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import PostWriteCancelModal from './PostWriteCancelModal/PostWriteCancelModal';
 import PostWriteConfirmModal from './PostWriteConfirmModal/PostWriteConfirmModal';
+import {useRecoilState} from 'recoil';
+import {userId} from '../../Atom';
+import axios from 'axios';
 
 export default function PostWritePage() {
   const [title, setTitle] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
   const [content, setContent] = useState('');
+  // const [user, setUser] = useRecoilState(userId);
   const [isCancelModalOpen, setIsCancelModalOpen] = useState(false);
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState('');
 
   const location = useLocation();
   console.log(location);
+
+  function onClickSave(){
+    // postData(selectedOption, userId, title, content);
+    setIsConfirmModalOpen(true);
+  } 
+
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+  async function postData(boardIdx,userIdx,postContent, postName) {
+    try {
+      const response = await axios.post(`/api/post/create`,
+        JSON.stringify({boardIdx,userIdx,postContent, postName}),
+        { headers }
+      );
+      console.log('리턴', response);
+      alert('저장완료');
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   const handleSelectOption = (e) => {
     setSelectedOption(e.target.value);
   };
   // 게시판에서 글쓰기 버튼 누를 때 writepage로 링크타고 이동할텐데 이때 현재 있는 게시판 데이터 파라미터를 넘겨준 다음 이 데이터를 writepage에서 useParams로 받아서 useEffet setSelectedOption를 호출해주면 된다.
- 
+  
   return (
     <div>
       <PostWriteCancelModal
@@ -50,11 +78,11 @@ export default function PostWritePage() {
         </div>
         <div className='select-board'>
           <div className='select-box'>
-            <select name="fruits" onChange={handleSelectOption} value={selectedOption}>
-              <option value="apple">"C언어" 게시판</option>
-              <option value="orange">"JAVA" 게시판</option>
-              <option value="grape">"PYTHION" 게시판</option>
-              <option value="melon">"자유게시판"</option>
+            <select name="option" onChange={handleSelectOption} value={selectedOption}>
+              <option value="C">"C언어" 게시판</option>
+              <option value="JAVA">"JAVA" 게시판</option>
+              <option value="Python">"PYTHION" 게시판</option>
+              <option value="free">"자유게시판"</option>
             </select>
           </div>
         </div>
@@ -76,7 +104,7 @@ export default function PostWritePage() {
         <div className='write-post-container'>
           <button
             className='write-post'
-            onClick={() => setIsConfirmModalOpen(true)}>
+            onClick={onClickSave}>
               <img src={pencil} alt="엑박" className='pencil' />
               <img src={writePost} alt="엑박" className='write-post-text' />
           </button>
