5001e7d758c604326167da773d75aa9d5e1d14e2
stdleaf <stdleaf@gmail.com>
2022-08-05 12:25:45 +0900

    Feat: 필터 토글기능 구현

--- src/pages/LecturesPage/LecturesPage.js
+++ src/pages/LecturesPage/LecturesPage.js
@@ -7,11 +7,15 @@ import LowBar from "../LowBar/LowBar";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Lecture from "./Lecture/Lecture";
+import keywordMinusIcon from '../../assets/deleteFilterIcon.png';
 
 export default function LecturesPage() {
   const [lectures, setLectures] = useState(null);
   const [error, setError] = useState(false);
   const [loading, isLoading] = useState(false);
+  const [subject, setSubject] = useState(null);
+  const [material, setMaterial] = useState(null);
+  const [pricing, setPricing] = useState(null);
 
   const headers = {
     Accept: 'application/json',
@@ -32,6 +36,78 @@ export default function LecturesPage() {
     isLoading(false);
   };
   
+  const handleFrontClick = () => {
+    if (subject === null) {
+      setSubject('Front');
+      return;
+    }
+
+    setSubject(null);
+  };
+
+  const handleBackClick = () => {
+    if (subject === null) {
+      setSubject('Back');
+      return;
+    }
+
+    setSubject(null);
+  };
+
+  const handlePythonClick = () => {
+    if (subject === null) {
+      setSubject('Python');
+      return;
+    }
+
+    setSubject(null);
+  };
+
+  const handleCClick = () => {
+    if (subject === null) {
+      setSubject('C 언어');
+      return;
+    }
+
+    setSubject(null);
+  };
+
+  const handleBookClick = () => {
+    if (material === null) {
+      setMaterial('서적');
+      return;
+    }
+
+    setMaterial(null);
+  };
+
+  const handleVideoClick = () => {
+    if (material === null) {
+      setMaterial('영상');
+      return;
+    }
+
+    setMaterial(null);
+  };
+
+  const handleFreeClick = () => {
+    if (pricing === null) {
+      setPricing('무료');
+      return;
+    }
+
+    setPricing(null);
+  };
+
+  const handleCostClick = () => {
+    if (pricing === null) {
+      setPricing('유료');
+      return;
+    }
+
+    setPricing(null);
+  };
+
   useEffect(() => {
     fetchLectures();
   }, []);
@@ -53,42 +129,58 @@ export default function LecturesPage() {
         <Search />
         <div className="keywords-container">
           <div className="keywords-subject-container">
-            <div className="keyword">
-              <div>Front</div>
-              <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" />
-            </div>
-            <div className="keyword">
-              <div>Front</div>
-              <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" />
-            </div>
-            <div className="keyword">
-              <div>Front</div>
-              <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" />
-            </div>
-            <div className="keyword">
-              <div>Front</div>
-              <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" />
-            </div>
+            {(subject === 'Front' || subject === null) && <button className={subject === 'Front' ? 'keyword red' : 'keyword'} onClick={handleFrontClick}>
+              Front
+              {subject === 'Front' ?
+                <img className="keyword-minus-icon" src={keywordMinusIcon} alt="" onClick={handleFrontClick}/> :
+              <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" onClick={handleFrontClick}/>}
+            </button> }
+            {(subject === 'Back' || subject === null) && <button className={subject === 'Back' ? 'keyword green' : 'keyword'} onClick={handleBackClick}>
+              <div>Back</div>
+              {subject === 'Back' ?
+                <img className="keyword-minus-icon" src={keywordMinusIcon} alt=""  onClick={handleBackClick}/> : 
+                <img className="keyword-plus-icon" src={keywordPlusIcon} alt=""  onClick={handleBackClick}/>}
+            </button> }
+            {(subject === 'Python' || subject === null) && <button className={subject === 'Python' ? 'keyword blue': 'keyword'} onClick={handlePythonClick}>
+              <div>Python</div>
+              {subject === 'Python' ?
+                <img className="keyword-minus-icon" src={keywordMinusIcon} alt="" onClick={handlePythonClick}/> :
+                <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" onClick={handlePythonClick}/>}
+            </button>}
+            {(subject === 'C 언어' || subject === null) && <button className={subject === 'C 언어' ? 'keyword gray': 'keyword'} onClick={handleCClick}>
+              <div>C 언어</div>
+              {subject === 'C 언어' ? 
+                <img className="keyword-minus-icon" src={keywordMinusIcon} alt="" onClick={handleCClick} /> :
+                <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" onClick={handleCClick} />}
+            </button>}
           </div>
           <div className="keywords-subject-container">
-            <div className="keyword">
-              Front
-              <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" />
-            </div>
-            <div className="keyword">
-              Front
-              <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" />
-            </div>
+          {(material === '서적' || material === null) && <button className={material === '서적' ? 'keyword white': 'keyword'} onClick={handleBookClick}>
+              서적
+              {material === '서적' ? 
+                <img className="keyword-minus-icon" src={keywordMinusIcon} alt="" onClick={handleBookClick} /> :
+                <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" onClick={handleBookClick} />}
+            </button>}
+            {(material === '영상' || material === null) && <button className={material === '영상' ? 'keyword white': 'keyword'} onClick={handleVideoClick}>
+              영상
+              {material === '영상' ? 
+                <img className="keyword-minus-icon" src={keywordMinusIcon} alt="" onClick={handleVideoClick} /> :
+                <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" onClick={handleVideoClick} />}
+            </button>}
           </div>
           <div>
-            <div className="keyword">
-              Front
-              <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" />
-            </div>
-            <div className="keyword">
-              Front
-              <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" />
-            </div>
+          {(pricing === '무료' || pricing === null) && <button className={pricing === '무료' ? 'keyword darkgray': 'keyword'} onClick={handleFreeClick}>
+              무료
+              {pricing === '무료' ? 
+                <img className="keyword-minus-icon" src={keywordMinusIcon} alt="" onClick={handleFreeClick} /> :
+                <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" onClick={handleFreeClick} />}
+            </button>}
+            {(pricing === '유료' || pricing === null) && <button className={pricing === '유료' ? 'keyword darkgray': 'keyword'} onClick={handleCostClick}>
+              유료
+              {pricing === '유료' ? 
+                <img className="keyword-minus-icon" src={keywordMinusIcon} alt="" onClick={handleCostClick} /> :
+                <img className="keyword-plus-icon" src={keywordPlusIcon} alt="" onClick={handleCostClick} />}
+            </button>}
           </div>
         </div>
         <div className="lecture-list">
