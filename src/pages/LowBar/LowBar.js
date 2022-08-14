26dce00c1dd1340e332c8e1bf63224e9ce5538ba
stdleaf <stdleaf@gmail.com>
2022-08-14 11:38:22 +0900

    Fix: 하단바 활성화 이슈 해결완

--- src/pages/LowBar/LowBar.js
+++ src/pages/LowBar/LowBar.js
@@ -1,19 +1,14 @@
-import { useEffect, useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import homeButton from '../../assets/homeButton.png';
 import activeHomeButton from '../../assets/activeHomeButton.png';
 import lectureButton from '../../assets/lectureButton.png';
-// import activeLectureButton from '../../assets/activeLectureButton.png';
+import activeLectureButton from '../../assets/lectureGreenIcon.png';
 import boardButton from '../../assets/boardButton.png';
 import activeBoardButton from '../../assets/activeBoardButton.png';
-import menuButton from '../../assets/menuButton.png';
-// import activeMenuButton from '../../assets/activeMenuButton.png';
-import curriculum1 from '../../assets/curriculumImg1.png';
-import curriculum2 from '../../assets/curriculumImg2.png';
+import activeCurriButton from '../../assets/activeCurriButton.png';
+import curriButton from '../../assets/curriButton.png';
 import "./LowBar.css";
-import lectureIcon1 from '../../assets/lectureIcon1.png';
-import lectureIcon2 from '../../assets/lectureIcon2.png';
-import lectureIcon3 from '../../assets/lectureIcon3.png';
 
 export default function LowBar() {
   const navigate = useNavigate();
@@ -22,7 +17,7 @@ export default function LowBar() {
     homeButton: false,
     lectureButton: false,
     boardButton: false,
-    menuButton: false
+    curriButton: false
   });
 
   const handleLowButtonClick = (e, path) => {
@@ -53,36 +48,15 @@ export default function LowBar() {
       </div>
       <div className="lecture-button" onClick={(e) => handleLowButtonClick(e, 'lectures')} id="lectureButton">
           <div className='lecture-icon-box' id="lectureButton">
-            {isButtonClicked.lectureButton ?
-                <div className='active-lecture-menu'>
-                  <div className='active-lecture-icon-box'>
-                    <img className='' src={lectureIcon1} alt="" />
-                  </div>
-                  <div className='active-lecture-icon-box'>
-                    <img src={lectureIcon2} alt="" />
-                  </div>
-                  <div className='active-lecture-icon-box'>
-                    <img src={lectureIcon3} alt="" />
-                  </div>
-                </div> :
-                <img id="lectureButton" src={lectureButton} alt=''/>}
+            <img id='lectureButton' src={isButtonClicked.lectureButton ? activeLectureButton : lectureButton} alt="" />
           </div>
           <div className={(isButtonClicked.lectureButton ? ' active-home-text' : 'home-text')} id='lectureButton'>강의</div>
       </div>
-      <div className="curriculum-button" onClick={(e) => handleLowButtonClick(e, '/curriculum')} id="menuButton">
-          <div className='curriculum-icon-box' id="menuButton">
-              {isButtonClicked.menuButton ?
-                <div className='curriculum-icons-box'>
-                  <div><img className='curriculum-icon1' src={curriculum1} alt="" /></div>
-                  <div><img src={curriculum2} alt="" /></div>
-                </div> :
-                <div className='curriculum-icons-box'>
-                  <div><img className='curriculum-icon1' src={curriculum1} alt="" /></div>
-                  <div><img src={curriculum2} alt="" /></div>
-                </div>
-              }
+      <div className="curriculum-button" onClick={(e) => handleLowButtonClick(e, '/curriculum')} id="curriButton">
+          <div className='curriculum-icon-box' id="curriButton">
+            <img id='curriButton' src={isButtonClicked.curriButton ? activeCurriButton : curriButton} alt="" />
           </div>
-          <div className={(isButtonClicked.menuButton ? ' active-home-text' : 'home-text')} id="menuButton">커리큘럼</div>
+          <div className={(isButtonClicked.curriButton ? ' active-home-text' : 'home-text')} id="curriButton">커리큘럼</div>
       </div>
       <div className="board-button" onClick={(e) => handleLowButtonClick(e, 'boards')} id="boardButton">
           <div className='board-icon-box' id="boardButton"><img id="boardButton" src={isButtonClicked.boardButton ? activeBoardButton : boardButton} alt=''/></div>
