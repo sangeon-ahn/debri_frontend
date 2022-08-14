7f6cedc426e5b60855d207114fd5696294fbc0b8
jinsil <jinsil378900@gmail.com>
2022-08-14 15:05:26 +0900

    Feat : 대댓글 좋아요 구현 완

--- src/pages/PostPage/ReComment/ReComment.js
+++ src/pages/PostPage/ReComment/ReComment.js
@@ -1,11 +1,62 @@
 import recommentArrow from '../../../assets/recommentArrow.png';
 import recommentMenuIcon from "../../../assets/commentMenuIcon.png";
 import grayUpThumb from '../../../assets/grayUpThumb.png';
+import greenUpThumb from '../../../assets/greenUpThumb.png';
+import { useState } from 'react';
+import axios from 'axios';
 
 import './ReComment.css';
 
 export default function ReComment(props) {
   const { reComment, setIsCommentSettingModalOn, setReportedComment } = props;
+  const [voteCount, setVoteCount] = useState(reComment.likeCount);
+  const [pressLike, setPressLike] = useState(reComment.likeStatus);
+  const [loading,setLoading] = useState(false); // 로딩되는지 여부
+  const [error,setError] = useState(null); //에러
+  const headers = {
+    'ACCESS-TOKEN': `${JSON.parse(localStorage.getItem("userData")).jwt}`,
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  //좋아요 생성
+  async function createLike(commentIdx) {
+    try {
+      const response = await axios.post(`/api/comment/like/create/${commentIdx}`,
+        JSON.stringify({}),
+        { headers }
+      );
+      console.log('좋아요 생성', response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //좋아요 취소
+  async function cancelLike(commentIdx) {
+    try {
+      const response = await axios.patch(`/api/comment/like/delete/${commentIdx}`,
+        JSON.stringify({}),
+        { headers }
+      );
+      console.log('좋아요 취소', response);
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    }
+  }
+
+  function onLike(e) {
+    createLike(e);
+    setVoteCount(voteCount+1);
+    setPressLike(true);
+  }
+
+  function onCancelLike(e) {
+    cancelLike(e);
+    setVoteCount(voteCount-1);
+    setPressLike(false);
+  }
 
   const handleRecommentMenuButtonClick = () => {
     setReportedComment(reComment);
@@ -23,8 +74,11 @@ export default function ReComment(props) {
           <div className="recomment-user-name">{reComment.authorName} &gt;</div>
           <div className='recomment-elapsed-time'>0분 전</div>
           <div className='recomment-button-box'>
-            <img src={grayUpThumb} alt='' className="gray-upthumb-icon" />
-            <div className='up-vote-number'>0</div>
+          {pressLike ? 
+              <img src={greenUpThumb} alt='' className="green-upthumb-icon" onClick={()=> onCancelLike(reComment.commentIdx)} style={{ margin:'-1px 8.49px 1px 0'}}/> :
+              <img src={grayUpThumb} alt='' className="gray-upthumb-icon" onClick={()=> onLike(reComment.commentIdx)} />
+            }   
+            <div className="up-vote-number">{voteCount}</div>
             <div className='barrier-line'></div>
             <button className='recomment-menu-button' onClick={handleRecommentMenuButtonClick}>
               <img src={recommentMenuIcon} alt="" className="recomment-menu-icon"  />
